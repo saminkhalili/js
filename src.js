93e0fa57ca8be1d552a9fe71c2b3198ea3aff556
samin <samin4business@gmail.com>
2024-07-16 08:42:08 -0700

    12

--- src.js
+++ src.js
@@ -1,8 +1,10 @@
-// console.log('from js 3');
-const getMessage = () =>"<h1>it is working1</h1>";
+// // console.log('from js 3');
+const getMessage = () =>"<h3>it is working1</h3>";
 document.body.innerHTML = getMessage();
 /*multiple 
 comment*/
+
 let message0 = "samin";
-console.log(message);
-// let message1 = "khalili";console.log(message);
+console.log(message0);
+
+let message1 = "khalili";console.log(message1);
