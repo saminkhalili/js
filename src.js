67e3541139a504241217e8ae0754042a5c644060
samin <samin4business@gmail.com>
2024-07-18 07:04:32 -0700

    lesson 4

--- src.js
+++ src.js
@@ -1,3 +1,4 @@
+//lesson 1+2+3
 // // console.log('from js 3');
 
 const getMessage = () =>"<h3>it is working1</h3>";
@@ -10,3 +11,49 @@ let message0 = "samin";
 console.log(message0);
 
 let message1 = "khalili";console.log(message1);
+
+
+//متغیر ها و ثابت ها در جاوا اسکریپت lesson 4
+let lenght = 10;
+let witth = 10;
+let area = lenght * witth;
+console.log(area);
+area = 200;
+console.log(area);
+let firstName='samin'; //cammel case writing
+let lastName='khalili';
+console.log(firstName +'  '+ lastName);
+
+const pi =3.14; // when u use const u can not change the value of pi 
+let lenght1 = 10;
+let witth1 = 10 * pi;
+let area2 = pi*lenght1*witth1;
+
+/*
+primitive Date Types
+1.number
+2.string
+3.boolean
+4.undefined
+5.Null
+6.symbol  >>>> no exmple below
+*/
+
+let a =20;
+console.log(typeof a);
+
+a= 'a';
+console.log(typeof a);//the type of a can change middle of proggram!
+
+let b =false;
+console.log(typeof b);
+
+let c ="asd";
+console.log(typeof c);
+
+let d ;
+console.log(typeof d);
+
+let e =null;
+console.log(typeof e);
+
